9dc18ce1f53c255c3ffbbbdf953184af302d82f1
Bianca BBB <biancabezz@icloud.com>
2026-03-30 21:05:30 -0300

    BackEnd

---
+++ BackEnd/src/test/java/com/senac/back/Back/End/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.back.Back.End;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
