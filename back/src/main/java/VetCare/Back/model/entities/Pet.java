c6e0f511ae5a21422d3d6fbdc89494d7d6df7569
Bianca BBB <biancabezz@icloud.com>
2026-04-09 12:56:15 -0300

    fix: Pet com Lombok

--- back/src/main/java/VetCare/Back/model/entities/Pet.java
+++ back/src/main/java/VetCare/Back/model/entities/Pet.java
@@ -1,35 +1,32 @@
 package VetCare.Back.model.entities;
 
-import VetCare.Back.model.enuns.EnumSexoPet;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-
-import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(name = "pets")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Pet {
-
-    public Pet() {}
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String nome;
     private String especie;
     private String raca;
     private Integer idade;
-    private BigDecimal peso;
-
-    @Enumerated(EnumType.STRING)
-    private EnumSexoPet sexo = EnumSexoPet.NAO_INFORMADO;
-
+    private Double peso;
+    private String sexo;
     private String cor;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "tutor_id", nullable = false)
     private Tutor tutor;
 
@@ -40,94 +37,4 @@ public class Pet {
     @OneToMany(mappedBy = "pet", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonIgnore
     private List<Prontuario> prontuarios = new ArrayList<>();
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEspecie() {
-        return especie;
-    }
-
-    public void setEspecie(String especie) {
-        this.especie = especie;
-    }
-
-    public String getRaca() {
-        return raca;
-    }
-
-    public void setRaca(String raca) {
-        this.raca = raca;
-    }
-
-    public Integer getIdade() {
-        return idade;
-    }
-
-    public void setIdade(Integer idade) {
-        this.idade = idade;
-    }
-
-    public BigDecimal getPeso() {
-        return peso;
-    }
-
-    public void setPeso(BigDecimal peso) {
-        this.peso = peso;
-    }
-
-    public EnumSexoPet getSexo() {
-        return sexo;
-    }
-
-    public void setSexo(EnumSexoPet sexo) {
-        this.sexo = sexo;
-    }
-
-    public String getCor() {
-        return cor;
-    }
-
-    public void setCor(String cor) {
-        this.cor = cor;
-    }
-
-    public Tutor getTutor() {
-        return tutor;
-    }
-
-    public void setTutor(Tutor tutor) {
-        this.tutor = tutor;
-    }
-
-    public List<Agendamento> getAgendamentos() {
-        return agendamentos;
-    }
-
-    public void setAgendamentos(List<Agendamento> agendamentos) {
-        this.agendamentos = agendamentos;
-    }
-
-    public List<Prontuario> getProntuarios() {
-        return prontuarios;
-    }
-
-    public void setProntuarios(List<Prontuario> prontuarios) {
-        this.prontuarios = prontuarios;
-    }
 }
-
-
