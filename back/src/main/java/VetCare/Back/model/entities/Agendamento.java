023c312f493dea88e86795397f73fd74e57444c3
Bianca BBB <biancabezz@icloud.com>
2026-04-09 12:56:16 -0300

    fix: Agendamento com Lombok e EnumStatus

--- back/src/main/java/VetCare/Back/model/entities/Agendamento.java
+++ back/src/main/java/VetCare/Back/model/entities/Agendamento.java
@@ -1,96 +1,34 @@
 package VetCare.Back.model.entities;
 
 import VetCare.Back.model.enuns.EnumStatusAgendamento;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "agendamentos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Agendamento {
-
-    public Agendamento() {}
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "data_hora", nullable = false)
-    private LocalDateTime dataHora;
-
-    @Enumerated(EnumType.STRING)
-    private EnumStatusAgendamento status = EnumStatusAgendamento.AGENDADO;
-
-    private String observacoes;
-
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "pet_id", nullable = false)
     private Pet pet;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "veterinario_id", nullable = false)
     private Veterinario veterinario;
 
-    @OneToOne(mappedBy = "agendamento", fetch = FetchType.EAGER)
-    @JsonIgnore
-    private Prontuario prontuario;
+    private LocalDateTime dataHora;
+    private String observacoes;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getDataHora() {
-        return dataHora;
-    }
-
-    public void setDataHora(LocalDateTime dataHora) {
-        this.dataHora = dataHora;
-    }
-
-    public EnumStatusAgendamento getStatus() {
-        return status;
-    }
-
-    public void setStatus(EnumStatusAgendamento status) {
-        this.status = status;
-    }
-
-    public String getObservacoes() {
-        return observacoes;
-    }
-
-    public void setObservacoes(String observacoes) {
-        this.observacoes = observacoes;
-    }
-
-    public Pet getPet() {
-        return pet;
-    }
-
-    public void setPet(Pet pet) {
-        this.pet = pet;
-    }
-
-    public Veterinario getVeterinario() {
-        return veterinario;
-    }
-
-    public void setVeterinario(Veterinario veterinario) {
-        this.veterinario = veterinario;
-    }
-
-    public Prontuario getProntuario() {
-        return prontuario;
-    }
-
-    public void setProntuario(Prontuario prontuario) {
-        this.prontuario = prontuario;
-    }
+    @Enumerated(EnumType.STRING)
+    private EnumStatusAgendamento status = EnumStatusAgendamento.AGENDADO;
 }
-
-
