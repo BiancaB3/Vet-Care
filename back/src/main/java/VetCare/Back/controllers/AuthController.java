97c14a90438f7484222db0eee95d27de62ab5878
Bianca BBB <biancabezz@icloud.com>
2026-04-09 12:57:17 -0300

    fix: AuthController com Swagger

--- back/src/main/java/VetCare/Back/controllers/AuthController.java
+++ back/src/main/java/VetCare/Back/controllers/AuthController.java
@@ -1,28 +1,28 @@
 package VetCare.Back.controllers;
 
-
-import VetCare.Back.model.DTO.LoginRequest;
-import VetCare.Back.model.DTO.LoginResponse;
-import org.springframework.http.HttpStatus;
+import VetCare.Back.model.entities.Veterinario;
+import VetCare.Back.model.repository.VeterinarioRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
+@Tag(name = "Auth controller", description = "Controladora responsável pela autenticação!")
 public class AuthController {
 
+    @Autowired
+    private VeterinarioRepository veterinarioRepository;
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest){
-
-
-        if(loginRequest.email().equals("String@s") &&  loginRequest.senha().equals("String")){
-            return ResponseEntity.ok(new LoginResponse("Sasdasdas123"));
+    @Operation(summary = "Login", description = "Método responsável por autenticar o veterinário!")
+    public ResponseEntity<?> login(@RequestBody Veterinario veterinario) {
+        var vet = veterinarioRepository.findByEmail(veterinario.getEmail()).orElse(null);
+        if (vet == null || !vet.getSenha().equals(veterinario.getSenha())) {
+            return ResponseEntity.status(401).body("Email ou senha inválidos!");
         }
-
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        return ResponseEntity.ok(vet);
     }
-
-
 }
-
