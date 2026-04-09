35a6cd77356efc0ac1e3f9c342e29d68506adced
Bianca BBB <biancabezz@icloud.com>
2026-04-09 12:56:34 -0300

    fix: AgendamentoRepository

--- back/src/main/java/VetCare/Back/model/repository/AgendamentoRepository.java
+++ back/src/main/java/VetCare/Back/model/repository/AgendamentoRepository.java
@@ -1,10 +1,6 @@
 package VetCare.Back.model.repository;
-
 import VetCare.Back.model.entities.Agendamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 @Repository
-public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
-}
-
+public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {}
