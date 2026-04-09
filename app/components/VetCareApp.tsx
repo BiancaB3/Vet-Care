1ed321f6005da7b7dc00167ef0844cd367539cf5
BiancaB3 <biancabezz@icloud.com>
2026-04-09 13:53:24 -0300

    Fix Login page
    Correção da login page e package versions

--- app/components/VetCareApp.tsx
+++ app/components/VetCareApp.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
-import { api } from '../../lib/api';
+import { api } from '../lib/api';
 import { useAuth } from '../context/AuthContext';
 import {
   Mail, Lock, Bell, LogOut, Calendar, Users, Dog, ClipboardList,
