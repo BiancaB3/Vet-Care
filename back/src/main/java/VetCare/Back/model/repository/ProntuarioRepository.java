76ff58a70b915901155ba50b46b7d25e808e220d
Bianca BBB <biancabezz@icloud.com>
2026-04-09 12:56:35 -0300

    fix: ProntuarioRepository

--- back/src/main/java/VetCare/Back/model/repository/ProntuarioRepository.java
+++ back/src/main/java/VetCare/Back/model/repository/ProntuarioRepository.java
@@ -1,10 +1,6 @@
 package VetCare.Back.model.repository;
-
 import VetCare.Back.model.entities.Prontuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 @Repository
-public interface ProntuarioRepository extends JpaRepository<Prontuario, Long> {
-}
-
+public interface ProntuarioRepository extends JpaRepository<Prontuario, Long> {}
